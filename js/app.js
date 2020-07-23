4fb3a3999597e205c471e55862f035b80ad6cc56
GerezPablo <pablogerez2000@gmail.com>
2020-07-23 20:40:11 -0300

    Habemus primera fila

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Parametros para formar la query
 var lat = -34.5708;
 var lon = -58.6243;
 var apiKey = "834075ea43292b8f4f1b3021724ad698";
@@ -7,30 +8,21 @@ var part = "";
 
 const request = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${unit}&lang=${lang}&exclude=${part}&appid=${apiKey}`;
 
-function getCurrentWeather() {    
+function getCurrentWeather() {    //Re distribuida la forma de llenar datos para hacer un solo llamado 
     return fetch(request).then(response => response.json()) //Le pega a la API.
     .then(weatherInfo => {
+        //Get City
+        const tz = weatherInfo.timezone.split("/");
+        document.getElementById("city").innerHTML = tz[tz.length-1];
+
+        //Current Temperatute
+        const ct = parseInt(weatherInfo.current.temp);        
+        document.getElementById("temperatura").innerHTML = ct;
+
         return weatherInfo.daily[0] //Devuelve dia actual.
     })
     .catch(err => { err = new Error(), console.log(err) })
 }
-
-function getCity() {
-    fetch(request).then(response => response.json())
-        .then(weatherInfo => { 
-        const tz = weatherInfo.timezone.split("/");
-        document.getElementById("city").innerHTML = tz[tz.length-1];
-    })
-}
-
-function getCurrentTemp() {
-    fetch(request).then(response => response.json())
-        .then(weatherInfo => { 
-        const ct = parseInt(weatherInfo.current.temp);        
-        document.getElementById("temperatura").innerHTML = ct;
-    })
-}
-
 function writeCurrentWeather() {
     getCurrentWeather()
     .then( day => {
@@ -38,9 +30,6 @@ function writeCurrentWeather() {
         const dayTime = new Date(day.dt * 1000);
         document.getElementById("dayTime").innerHTML =  dayTime.toString().substr(0,21);;
         
-        //city
-        getCity()
-
         //weather icon  
         const icon = day.weather[0].icon;
         const url = ` http://openweathermap.org/img/wn/${icon}@4x.png`;
@@ -51,21 +40,12 @@ function writeCurrentWeather() {
         console.log( `${currentSunrise.getHours()}:${currentSunrise.getMinutes()}` );
 
         //Sunset
-        document.getElementById("fecha").innerHTML = `${currentSunrise.getHours()}:${currentSunrise.getMinutes()}`
-
-
-        //Temps 
-        // =>Current
-        getCurrentTemp();
-
         const currentSunset = new Date(day.sunset * 1000);
         console.log( `${currentSunset.getHours()}:${currentSunset.getMinutes()}` );
         
-        
-        console.log(`
-        Max: ${parseInt(day.temp.max)}\n
-        Min: ${parseInt(day.temp.min)}
-        `)
+        //Min & max
+        document.getElementById("max").innerHTML = `${parseInt(day.temp.max)}°C↑`;
+        document.getElementById("min").innerHTML = `${parseInt(day.temp.min)}°C↓`;
     })
 }
 /*
@@ -74,7 +54,5 @@ function writeCurrentWeather() {
 "humidity":87,"dew_point":285.51,"uvi":2.84,"clouds":90,"visibility":5000,"wind_speed":2.6,"wind_deg":230,
 "weather":[{"id":701,"main":"Mist","description":"mist","icon":"50n"}]}}
 */
-
-//http://openweathermap.org/img/wn/10d@2x.png HABEMUS ICONS
 
 writeCurrentWeather() 
